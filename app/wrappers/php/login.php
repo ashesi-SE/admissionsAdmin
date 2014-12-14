2848dccb4518cc6e4d5d6e05baa8554c4dc144d1
Niena <niena.alhassan@gmail.com>
2014-12-14 20:13:29 +0000

    Log 1st function

--- app/wrappers/php/login.php
+++ app/wrappers/php/login.php
@@ -25,6 +25,14 @@
             //call login to check username and password
             if (login($username, $password)) {
                 session_start(); //initiate session for the current login
+				$type="login";
+				$message="Successful";
+				//$ip=$_SERVER['REMOTE_ADDR'];//"10.10.21.234";
+				$ip=get_client_ip();
+				$hostName="local";
+				$uri=$_SERVER['REQUEST_URI'];
+				$refer="refer";
+				addtoLog($type,$message,$ip,$hostName,$uri,$refer);
                 loadUserProfile($username); //load user information into the session
                 header("location: admissions.php"); //redirect to home page
                 echo "<a href='gridIndex.php'>click here</a>"; //if redirect fails, provide a link
@@ -80,11 +88,11 @@
 function login($username, $password) {
 
 //    $pword = md5($password);
-    $link = mysql_pconnect("localhost", "root", "Dream1234") or die("Unable To Connect To Database Server");
+    $link = mysql_pconnect("localhost", "root", "") or die("Unable To Connect To Database Server");
     $db = mysql_select_db("ashadmission") or die("Unable To Connect To Admissions");
 
 //    DEFINE('DBUSER', 'root');
-//    DEFINE('DBPW', 'Dream1234');
+//    DEFINE('DBPW', '');
 //    DEFINE('DBHOST', 'localhost');
 //    DEFINE('DBNAME', 'ashadmission');
 //
@@ -121,4 +129,42 @@ function loadUserProfile($username) {
     $_SESSION['username'] = $username;
     //$_SESSION['fullname'] = $fullname;
 }
+
+function addtoLog($type,$message,$ip,$hostName,$uri,$refer){
+$link = mysql_pconnect("localhost", "root", "") or die("Unable To Connect To Database Server");
+    $db = mysql_select_db("ashadmission") or die("Unable To Connect To Admissions");
+	if(mysql_query("INSERT INTO `logs`(`type`, `message`, `created`, `ip`, `hostname`, `uri`, `refer`) VALUES ('$type','$message',now(),'$ip','$hostName','$uri','$refer')")) 
+{
+          return true;
+    }else{
+         echo mysql_error();
+        return false;
+    }
+
+
+
+
+}
+
+ function get_client_ip()
+ {
+      $ipaddress = '';
+      if (getenv('HTTP_CLIENT_IP'))
+          $ipaddress = getenv('HTTP_CLIENT_IP');
+      else if(getenv('HTTP_X_FORWARDED_FOR'))
+          $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+      else if(getenv('HTTP_X_FORWARDED'))
+          $ipaddress = getenv('HTTP_X_FORWARDED');
+      else if(getenv('HTTP_FORWARDED_FOR'))
+          $ipaddress = getenv('HTTP_FORWARDED_FOR');
+      else if(getenv('HTTP_FORWARDED'))
+          $ipaddress = getenv('HTTP_FORWARDED');
+      else if(getenv('REMOTE_ADDR'))
+          $ipaddress = getenv('REMOTE_ADDR');
+      else
+          $ipaddress = 'UNKNOWN';
+
+      return $ipaddress;
+ }
+
 ?>
